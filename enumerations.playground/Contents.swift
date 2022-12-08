ec9fdb4ef0d57664092117abf485c8f8818768b3
Jairo Jair Toro Novellis <jtnovellis88@gmail.com>
2022-12-08 03:40:06 -0500

    enumerations

--- enumerations.playground/Contents.swift
+++ enumerations.playground/Contents.swift
@@ -1,3 +1,29 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+enum Animals {
+    case cat
+    case dog
+    case rabbit
+}
+
+let cat = Animals.cat
+
+
+if cat == Animals.cat {
+    "This is a cat"
+} else if cat == Animals.dog {
+    "this is a dog"
+} else  {
+    "this is somthing else"
+}
+
+switch cat {
+case .cat:
+    "This is a cat"
+    break
+case .dog:
+    "This is a dog"
+    break
+default:
+    "this is something else"
+}
