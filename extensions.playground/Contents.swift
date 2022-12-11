c2c065c4d415ba5b7453c117b05e283a69b7203e
Jairo Jair Toro Novellis <jtnovellis88@gmail.com>
2022-12-11 17:34:22 -0500

    extensions lesson done

--- extensions.playground/Contents.swift
+++ extensions.playground/Contents.swift
@@ -1,1 +1,83 @@
 import Foundation
+
+// essencially we can extends existing types, adding them functionalities
+
+extension Int {
+    func plusTwo()-> Int {
+        self + 2
+    }
+}
+
+let two = 2
+two.plusTwo()
+
+struct Person {
+    let firstName: String
+    let lastName: String
+}
+
+extension Person {
+    init(fullName: String) {
+        let components = fullName.components(separatedBy: " ")
+        self.init(
+            firstName: components.first ?? fullName,
+            lastName: components.last ?? fullName
+        )
+    }
+}
+
+let person = Person(fullName: "Foo Bar")
+person.firstName
+person.lastName
+
+
+
+
+protocol GoesVroom {
+    var vroomValue: String { get }
+    func goVroom() -> String
+}
+
+extension GoesVroom {
+    func goVroom() -> String {
+        "\(self.vroomValue) goes vroom"
+    }
+}
+
+struct Car {
+    let manufacturer: String
+    let model: String
+}
+
+let modelX = Car(manufacturer: "Tesla", model: "X")
+
+extension Car: GoesVroom {
+    var vroomValue: String {
+        "\(self.manufacturer) model \(self.model)"
+    }
+}
+
+modelX.goVroom()
+
+
+
+
+
+
+
+class MyDouble {
+    var value: Double
+    init(value: Double) {
+        self.value = value
+    }
+}
+
+extension MyDouble {
+    convenience init() {
+        self.init(value: 0)
+    }
+}
+
+let myDouble = MyDouble()
+
+myDouble.value
